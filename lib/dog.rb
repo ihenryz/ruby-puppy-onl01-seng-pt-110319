0b02c19af9532cea2daef61212a80a6515031ebe
Henry Phan <h.phan06@gmail.com>
2019-12-01 21:15:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,33 +34,47 @@
 
 require 'pry'
 
-class Dog 
+class Dog   
 
   attr_accessor :name
-  
   @@all = []
-
+  
   def initialize(name)
     @name = name
     self.save
   end
   
   def self.all
-    @@all
-  end
-  
-  def self.clear_all
-    @@all.clear
+    @@all.each {|dog|}
   end
   
   def self.print_all
-    @@all.each do |dog|
-      puts dog.name
-    end
+    self.all.each {|dog| puts dog.name}
   end
   
   def save
-    self.class.all << self
-  end
+    @@all << self
+  end 
   
+  def self.clear_all 
+    @@all.clear
+  end 
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
